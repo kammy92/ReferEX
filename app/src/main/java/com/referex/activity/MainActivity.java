8f2caaef1c0d47526b9e2da20a09f3bc50f7ce47
actimobiledev <actiknow.mobiledev@gmail.com>
2017-07-10 18:26:32 +0530

    login screen update

--- app/src/main/java/com/referex/activity/MainActivity.java
+++ app/src/main/java/com/referex/activity/MainActivity.java
@@ -1,7 +1,9 @@
-package com.referex;
+package com.referex.activity;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+
+import com.referex.R;
 
 public class MainActivity extends AppCompatActivity {
     
