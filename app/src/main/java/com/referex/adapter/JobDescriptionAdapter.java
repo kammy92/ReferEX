10f5c996f5db61358e8bb845b400fc5adc92113d
actimobiledev <actiknow.mobiledev@gmail.com>
2017-07-24 11:36:28 +0530

    update

---
+++ app/src/main/java/com/referex/adapter/JobDescriptionAdapter.java
@@ -0,0 +1,111 @@
+package com.referex.adapter;
+
+import android.app.Activity;
+import android.support.annotation.NonNull;
+import android.support.v7.widget.RecyclerView;
+import android.util.Log;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.CheckBox;
+import android.widget.EditText;
+import android.widget.ProgressBar;
+import android.widget.TextView;
+
+
+import com.referex.R;
+import com.referex.model.JobDescription;
+import com.referex.utils.Utils;
+
+import org.json.JSONObject;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.Hashtable;
+import java.util.List;
+import java.util.Map;
+
+
+public class JobDescriptionAdapter extends RecyclerView.Adapter<JobDescriptionAdapter.ViewHolder> {
+    OnItemClickListener mItemClickListener;
+
+    private Activity activity;
+    private List<JobDescription> jobDescriptions = new ArrayList<JobDescription>();
+
+    public JobDescriptionAdapter(Activity activity, List<JobDescription> jobDescriptions) {
+        this.activity = activity;
+        this.jobDescriptions = jobDescriptions;
+    }
+    
+    @Override
+    public ViewHolder onCreateViewHolder (ViewGroup parent, int viewType) {
+        final LayoutInflater mInflater = LayoutInflater.from (parent.getContext ());
+        final View sView = mInflater.inflate (R.layout.list_item_job_listing, parent, false);
+        return new ViewHolder (sView);
+    }
+    
+    @Override
+    public void onBindViewHolder (final ViewHolder holder, int position) {//        runEnterAnimation (holder.itemView);
+
+        final JobDescription jobDescription = jobDescriptions.get (position);
+    
+        Utils.setTypefaceToAllViews (activity, holder.tvCompanyName);
+        Utils.setTypefaceToAllViews (activity, holder.tvExperience);
+        Utils.setTypefaceToAllViews (activity, holder.tvJobName);
+        Utils.setTypefaceToAllViews (activity, holder.tvLocation);
+        Utils.setTypefaceToAllViews (activity, holder.tvSkill);
+
+        holder.tvCompanyName.setText(jobDescription.getCompany_name());
+        holder.tvJobName.setText(jobDescription.getJob_name());
+        holder.tvLocation.setText(jobDescription.getLocation());
+        holder.tvSkill.setText(jobDescription.getSkill());
+        holder.tvExperience.setText(jobDescription.getExperience());
+
+
+    }
+    
+    @Override
+    public int getItemCount () {
+        return jobDescriptions.size ();
+    }
+    
+    public void SetOnItemClickListener (final OnItemClickListener mItemClickListener) {
+        this.mItemClickListener = mItemClickListener;
+    }
+    
+
+
+    public interface OnItemClickListener {
+        public void onItemClick (View view, int position);
+    }
+    
+    public class ViewHolder extends RecyclerView.ViewHolder implements View.OnClickListener {
+        TextView tvJobName;
+        TextView tvCompanyName;
+        TextView tvExperience;
+        TextView tvLocation;
+        TextView tvSkill;
+
+        ProgressBar progressBar;
+
+        
+        public ViewHolder (View view) {
+            super (view);
+            tvJobName = (TextView) view.findViewById (R.id.tvJobDescription);
+            tvCompanyName = (TextView) view.findViewById (R.id.tvCompanyName);
+            tvExperience = (TextView) view.findViewById (R.id.tvExperience);
+            tvLocation = (TextView) view.findViewById (R.id.tvLocation);
+            tvSkill = (TextView) view.findViewById (R.id.tvSkill);
+
+            view.setOnClickListener (this);
+        }
+        
+        @Override
+        public void onClick (View v) {
+
+            
+        }
+    }
+}
+
+
---
+++ app/src/main/java/com/referex/model/JobDescription.java
@@ -0,0 +1,75 @@
+package com.referex.model;
+
+import org.json.JSONObject;
+
+/**
+ * Created by Rahul jain on 21-07-2017.
+ */
+
+public class JobDescription {
+    int id;
+    String job_name,experience,skill,company_name,location;
+
+
+
+    public JobDescription(int id,String job_name,String company_name,String experience,String location,String skill) {
+        this.id=id;
+        this.job_name=job_name;
+        this.experience=experience;
+        this.skill=skill;
+        this.company_name=company_name;
+        this.location=location;
+
+    }
+
+    public JobDescription() {
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getJob_name() {
+        return job_name;
+    }
+
+    public void setJob_name(String job_name) {
+        this.job_name = job_name;
+    }
+
+    public String getExperience() {
+        return experience;
+    }
+
+    public void setExperience(String experience) {
+        this.experience = experience;
+    }
+
+    public String getSkill() {
+        return skill;
+    }
+
+    public void setSkill(String skill) {
+        this.skill = skill;
+    }
+
+    public String getCompany_name() {
+        return company_name;
+    }
+
+    public void setCompany_name(String company_name) {
+        this.company_name = company_name;
+    }
+
+    public String getLocation() {
+        return location;
+    }
+
+    public void setLocation(String location) {
+        this.location = location;
+    }
+}
