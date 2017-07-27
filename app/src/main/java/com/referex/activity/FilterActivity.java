98b3f5f2bcdbd9b29b61638cf1b98349c4245a57
actimobiledev <actiknow.mobiledev@gmail.com>
2017-07-27 10:49:55 +0530

    update 26 june

---
+++ app/src/main/java/com/referex/activity/FilterActivity.java
@@ -0,0 +1,184 @@
+package com.referex.activity;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.RelativeLayout;
+import android.widget.Spinner;
+import android.widget.TextView;
+
+import com.afollestad.materialdialogs.MaterialDialog;
+import com.referex.R;
+import com.referex.utils.Utils;
+
+import java.util.ArrayList;
+import java.util.Collection;
+
+import mabbas007.tagsedittext.TagsEditText;
+
+/**
+ * Created by actiknow on 7/25/17.
+ */
+public class FilterActivity extends AppCompatActivity {
+    Spinner spSalary;
+    TagsEditText locationEditText;
+    TextView tvRole;
+    TextView tvEducation;
+    TextView tvFunctionalArea;
+    TextView tv1;
+    TextView tv2;
+    TextView tvApply;
+    TextView tvTitle;
+    TextView tvBack;
+
+    RelativeLayout rlBack;
+
+    EditText etEducation;
+    EditText etRole;
+    EditText etFunctionalArea;
+    ImageView ivFilter;
+    ArrayList<String> functionalAreaList = new ArrayList<>();
+    String[] salaryArray = {"0-3", "3-6", "8-10", "10-15", "15-40"};
+    ArrayList<String> roleList = new ArrayList<>();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_filter);
+        initView();
+        initData();
+        initListener();
+    }
+
+
+    private void initView() {
+        spSalary = (Spinner) findViewById(R.id.spSalary);
+        locationEditText = (TagsEditText) findViewById(R.id.locationEditText);
+        tvRole = (TextView) findViewById(R.id.tvRole);
+        tvEducation = (TextView) findViewById(R.id.tvEducation);
+        tvFunctionalArea = (TextView) findViewById(R.id.tvFunctionalArea);
+        tv1 = (TextView) findViewById(R.id.tv1);
+        tv2 = (TextView) findViewById(R.id.tv2);
+        tvApply = (TextView) findViewById(R.id.tvApply);
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvBack = (TextView) findViewById(R.id.tvBack);
+        rlBack=(RelativeLayout)findViewById(R.id.rlBack);
+
+        etEducation = (EditText) findViewById(R.id.etEducation);
+        etFunctionalArea = (EditText) findViewById(R.id.etFunctionalArea);
+        etRole = (EditText) findViewById(R.id.etRole);
+
+        Utils.setTypefaceToAllViews (this, etEducation);
+        Utils.setTypefaceToAllViews (this, etFunctionalArea);
+        Utils.setTypefaceToAllViews (this, etEducation);
+        Utils.setTypefaceToAllViews (this, spSalary);
+
+        Utils.setTypefaceToAllViews (this, tvRole);
+        Utils.setTypefaceToAllViews (this, tvEducation);
+        Utils.setTypefaceToAllViews (this, tvFunctionalArea);
+        Utils.setTypefaceToAllViews (this, tv1);
+        Utils.setTypefaceToAllViews (this, tv2);
+        Utils.setTypefaceToAllViews (this, tvApply);
+        Utils.setTypefaceToAllViews (this, tvBack);
+        Utils.setTypefaceToAllViews (this, tvTitle);
+
+    }
+
+
+    private void initData() {
+        ArrayAdapter<String> spinnerArrayAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_spinner_item, salaryArray); //selected item will look like a spinner set from XML
+        spinnerArrayAdapter.setDropDownViewResource(android.R.layout.simple_spinner_dropdown_item);
+        spSalary.setAdapter(spinnerArrayAdapter);
+
+
+        roleList.add("Programmer");
+        roleList.add("Designer");
+        roleList.add("Analyst");
+        roleList.add("Manager");
+
+
+        functionalAreaList.add("Software Engineer");
+        functionalAreaList.add("Java Developer");
+        functionalAreaList.add("Finance Management");
+        functionalAreaList.add("PHP Developer");
+        functionalAreaList.add("IOS Developer");
+    }
+
+    private void initListener() {
+        rlBack.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick (View v) {
+                finish ();
+                overridePendingTransition (R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
+
+
+
+
+        etFunctionalArea.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(FilterActivity.this)
+                        .title("Functional Area")
+                        .items(functionalAreaList)
+                        .itemsCallbackMultiChoice(null, new MaterialDialog.ListCallbackMultiChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, Integer[] which, CharSequence[] text) {
+                                String value = "";
+                                for (int i = 0; i < text.length; i++) {
+                                    if (i == 0) {
+                                        value = text[0].toString();
+                                    } else {
+                                        value = value + "," + text[i].toString();
+                                    }
+
+                                    etFunctionalArea.setText(value);
+                                    //Log.e("Text",""+text[i].toString()+",");
+                                }
+                                return true;
+                            }
+                        })
+                        .positiveText("Submit")
+                        .show();
+            }
+        });
+
+
+        etRole.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(FilterActivity.this)
+                        .title("Role")
+                        .items(roleList)
+                        .itemsCallbackMultiChoice(null, new MaterialDialog.ListCallbackMultiChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, Integer[] which, CharSequence[] text) {
+                                String value = "";
+                                for (int i = 0; i < text.length; i++) {
+                                    if (i == 0) {
+                                        value = text[0].toString();
+                                    } else {
+                                        value = value + "," + text[i].toString();
+                                    }
+
+                                    etRole.setText(value);
+                                    //Log.e("Text",""+text[i].toString()+",");
+                                }
+                                return true;
+                            }
+                        })
+                        .positiveText("Submit")
+                        .show();
+            }
+        });
+
+
+    }
+
+
+}
---
+++ app/src/main/java/com/referex/activity/UploadResumeActivity.java
@@ -0,0 +1,242 @@
+package com.referex.activity;
+
+import android.Manifest;
+import android.app.ProgressDialog;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Bundle;
+import android.support.annotation.NonNull;
+import android.support.v4.app.ActivityCompat;
+import android.support.v4.content.ContextCompat;
+import android.support.v7.app.AppCompatActivity;
+import android.util.Log;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ImageView;
+import android.widget.ListView;
+import android.widget.RelativeLayout;
+import android.widget.TextView;
+import android.widget.Toast;
+
+import com.referex.R;
+import com.referex.utils.AppConfigTags;
+import com.referex.utils.FilePath;
+import com.referex.utils.Utils;
+
+import net.gotev.uploadservice.MultipartUploadRequest;
+import net.gotev.uploadservice.UploadNotificationConfig;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Collection;
+import java.util.UUID;
+
+import mabbas007.tagsedittext.TagsEditText;
+
+/**
+ * Created by l on 25/07/2017.
+ */
+
+public class UploadResumeActivity extends AppCompatActivity implements TagsEditText.TagsEditListener{
+
+    TextView tvUploadResume;
+    TextView tvTitle;
+    TextView tvSelectResume;
+    EditText etName;
+    EditText etEmail;
+    EditText etMobile;
+    TagsEditText mTagsEditText;
+    TextView tvJobPosition;
+
+    public static final String UPLOAD_URL = "https://project-referex-cammy92.c9users.io/api/upload_new.php";
+    public static final String PDF_FETCH_URL = "http://internetfaqs.net/AndroidPdfUpload/getPdfs.php";
+    private int PICK_PDF_REQUEST = 1;
+    private static final int STORAGE_PERMISSION_CODE = 123;
+    private Uri filePath;
+    ProgressDialog progressDialog;
+    String jobPosition;
+
+    RelativeLayout rlBack;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_resume);
+        initView();
+        initData();
+        initListener();
+        getExtras();
+
+    }
+
+
+
+    private void getExtras() {
+        Intent intent = getIntent();
+        jobPosition = intent.getStringExtra(AppConfigTags.JOB_POSITION);
+        tvJobPosition.setText(jobPosition);
+
+    }
+
+
+    private void initData() {
+        requestStoragePermission();
+        progressDialog = new ProgressDialog(this);
+
+        String[] title = {"C", "C++", "Java", "Android", "Html", "Php", "Hadoop", "Tableau", "Ios"
+        };
+
+        mTagsEditText.setHint("Skill");
+        mTagsEditText.setTagsListener(UploadResumeActivity.this);
+        mTagsEditText.setTagsWithSpacesEnabled(true);
+        mTagsEditText.setAdapter(new ArrayAdapter<String>(this, android.R.layout.simple_dropdown_item_1line, title));
+        mTagsEditText.setThreshold(1);
+
+        Utils.setTypefaceToAllViews (this, tvSelectResume);
+        Utils.setTypefaceToAllViews (this, tvUploadResume);
+        Utils.setTypefaceToAllViews (this, tvTitle);
+    }
+
+    private void initView() {
+        tvTitle = (TextView) findViewById(R.id.tvTitle);
+        tvUploadResume = (TextView) findViewById(R.id.buttonUpload);
+        tvSelectResume = (TextView) findViewById(R.id.editTextName);
+        tvJobPosition = (TextView) findViewById(R.id.tvJobPosition);
+        etName = (EditText) findViewById(R.id.etName);
+        etEmail = (EditText) findViewById(R.id.etEmail);
+        etMobile = (EditText) findViewById(R.id.etMobile);
+        mTagsEditText = (TagsEditText) findViewById(R.id.tagsEditText);
+        rlBack=(RelativeLayout)findViewById(R.id.rlBack);
+
+
+        Utils.setTypefaceToAllViews (this, tvSelectResume);
+        Utils.setTypefaceToAllViews (this, tvUploadResume);
+        Utils.setTypefaceToAllViews (this, tvTitle);
+        Utils.setTypefaceToAllViews (this, etName);
+        Utils.setTypefaceToAllViews (this, etEmail);
+        Utils.setTypefaceToAllViews (this, etMobile);
+        Utils.setTypefaceToAllViews (this, mTagsEditText);
+
+
+
+
+    }
+
+    private void initListener() {
+        tvSelectResume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                showFileChooser();
+
+            }
+        });
+        tvUploadResume.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                uploadMultipart();
+
+            }
+        });
+
+        rlBack.setOnClickListener (new View.OnClickListener () {
+            @Override
+            public void onClick (View v) {
+                finish ();
+                overridePendingTransition (R.anim.slide_in_left, R.anim.slide_out_right);
+            }
+        });
+
+    }
+
+
+    public void uploadMultipart() {
+
+        String name = tvSelectResume.getText().toString().trim();
+        String path = FilePath.getPath(this, filePath);
+        tvSelectResume.setText(path);
+        if (path == null) {
+            Toast.makeText(this, "Please move your .pdf file to internal storage and retry", Toast.LENGTH_LONG).show();
+        } else {
+            try {
+                String uploadId = UUID.randomUUID().toString();
+                new MultipartUploadRequest(this, uploadId, UPLOAD_URL)
+                        .addFileToUpload(path, "pdf") //Adding file
+                        .addParameter("name", name)//Adding text parameter to the request
+                        .setNotificationConfig(new UploadNotificationConfig())
+                        .setMaxRetries(2)
+                        .startUpload(); //Starting the upload
+                tvSelectResume.setText("");
+            } catch (Exception exc) {
+                Toast.makeText(this, exc.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }
+    }
+
+
+    private void showFileChooser() {
+        Intent intent = new Intent();
+        intent.setType("application/*");
+        intent.addCategory(Intent.CATEGORY_OPENABLE);
+        intent.setAction(Intent.ACTION_GET_CONTENT);
+        try {
+            startActivityForResult(Intent.createChooser(intent, "Select a File to Upload"), PICK_PDF_REQUEST);
+        } catch (android.content.ActivityNotFoundException ex) {
+            Toast.makeText(UploadResumeActivity.this, "Please install a File Manager.", Toast.LENGTH_SHORT).show();
+        }
+    }
+
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == PICK_PDF_REQUEST && resultCode == RESULT_OK && data != null && data.getData() != null) {
+            filePath = data.getData();
+            tvSelectResume.setText("" + filePath);
+
+        }
+    }
+
+
+    //Requesting permission
+    private void requestStoragePermission() {
+        if (ContextCompat.checkSelfPermission(this, Manifest.permission.READ_EXTERNAL_STORAGE) == PackageManager.PERMISSION_GRANTED)
+            return;
+        if (ActivityCompat.shouldShowRequestPermissionRationale(this, Manifest.permission.READ_EXTERNAL_STORAGE)) {
+        }
+        ActivityCompat.requestPermissions(this, new String[]{Manifest.permission.READ_EXTERNAL_STORAGE}, STORAGE_PERMISSION_CODE);
+    }
+
+    @Override
+    public void onRequestPermissionsResult(int requestCode, @NonNull String[] permissions, @NonNull int[] grantResults) {
+        if (requestCode == STORAGE_PERMISSION_CODE) {
+            if (grantResults.length > 0 && grantResults[0] == PackageManager.PERMISSION_GRANTED) {
+                Toast.makeText(this, "Permission granted now you can read the storage", Toast.LENGTH_LONG).show();
+            } else {
+                Toast.makeText(this, "Oops you just denied the permission", Toast.LENGTH_LONG).show();
+            }
+        }
+    }
+
+    @Override
+    public void onTagsChanged (Collection<String> tags) {
+        Log.d ("chip add value", "Tags changed: ");
+        Log.d ("chip add value", Arrays.toString (tags.toArray ()));
+    }
+
+    @Override
+    public void onEditingFinished () {
+        Log.d ("Edit", "OnEditing finished");
+//        InputMethodManager imm = (InputMethodManager)getSystemService(Context.INPUT_METHOD_SERVICE);
+//        imm.hideSoftInputFromWindow(mTagsEditText.getWindowToken(), 0);
+//        //mTagsEditText.clearFocus();
+    }
+
+}
+
+
+
