5be18dc3eca3d54784aa689d006ca927ebff974b
actimobiledev <actiknow.mobiledev@gmail.com>
2017-07-27 14:44:24 +0530

    update 26 june

--- app/src/main/java/com/referex/activity/FilterActivity.java
+++ app/src/main/java/com/referex/activity/FilterActivity.java
@@ -1,10 +1,12 @@
 package com.referex.activity;
 
+import android.app.DatePickerDialog;
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.util.Log;
 import android.view.View;
 import android.widget.ArrayAdapter;
+import android.widget.DatePicker;
 import android.widget.EditText;
 import android.widget.ImageView;
 import android.widget.RelativeLayout;
@@ -16,6 +18,7 @@ import com.referex.R;
 import com.referex.utils.Utils;
 
 import java.util.ArrayList;
+import java.util.Calendar;
 import java.util.Collection;
 
 import mabbas007.tagsedittext.TagsEditText;
@@ -34,16 +37,17 @@ public class FilterActivity extends AppCompatActivity {
     TextView tvApply;
     TextView tvTitle;
     TextView tvBack;
-
     RelativeLayout rlBack;
-
     EditText etEducation;
     EditText etRole;
     EditText etFunctionalArea;
+    EditText etLocation;
     ImageView ivFilter;
     ArrayList<String> functionalAreaList = new ArrayList<>();
     String[] salaryArray = {"0-3", "3-6", "8-10", "10-15", "15-40"};
+
     ArrayList<String> roleList = new ArrayList<>();
+    ArrayList<String> cityList = new ArrayList<>();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -57,7 +61,7 @@ public class FilterActivity extends AppCompatActivity {
 
     private void initView() {
         spSalary = (Spinner) findViewById(R.id.spSalary);
-        locationEditText = (TagsEditText) findViewById(R.id.locationEditText);
+        etLocation = (EditText) findViewById(R.id.etLocation);
         tvRole = (TextView) findViewById(R.id.tvRole);
         tvEducation = (TextView) findViewById(R.id.tvEducation);
         tvFunctionalArea = (TextView) findViewById(R.id.tvFunctionalArea);
@@ -76,7 +80,6 @@ public class FilterActivity extends AppCompatActivity {
         Utils.setTypefaceToAllViews (this, etFunctionalArea);
         Utils.setTypefaceToAllViews (this, etEducation);
         Utils.setTypefaceToAllViews (this, spSalary);
-
         Utils.setTypefaceToAllViews (this, tvRole);
         Utils.setTypefaceToAllViews (this, tvEducation);
         Utils.setTypefaceToAllViews (this, tvFunctionalArea);
@@ -106,6 +109,24 @@ public class FilterActivity extends AppCompatActivity {
         functionalAreaList.add("Finance Management");
         functionalAreaList.add("PHP Developer");
         functionalAreaList.add("IOS Developer");
+
+        cityList.add("Ahemdabad");
+        cityList.add("Bangalore");
+        cityList.add("Bhopal");
+        cityList.add("Chennai");
+        cityList.add("Delhi");
+        cityList.add("Faridabad");
+        cityList.add("Gurgaon");
+        cityList.add("Ghaziabad");
+        cityList.add("Gandhinagar");
+        cityList.add("Grater Noida");
+        cityList.add("Hyderabad");
+        cityList.add("Indore");
+        cityList.add("Indore");
+        cityList.add("Kolkata");
+        cityList.add("Mumbai");
+        cityList.add("Pune");
+
     }
 
     private void initListener() {
@@ -116,9 +137,6 @@ public class FilterActivity extends AppCompatActivity {
                 overridePendingTransition (R.anim.slide_in_left, R.anim.slide_out_right);
             }
         });
-
-
-
 
         etFunctionalArea.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -136,8 +154,34 @@ public class FilterActivity extends AppCompatActivity {
                                     } else {
                                         value = value + "," + text[i].toString();
                                     }
-
                                     etFunctionalArea.setText(value);
+                                    //Log.e("Text",""+text[i].toString()+",");
+                                }
+                                return true;
+                            }
+                        })
+                        .positiveText("Submit")
+                        .show();
+            }
+        });
+
+        etLocation.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                new MaterialDialog.Builder(FilterActivity.this)
+                        .title("Select City")
+                        .items(cityList)
+                        .itemsCallbackMultiChoice(null, new MaterialDialog.ListCallbackMultiChoice() {
+                            @Override
+                            public boolean onSelection(MaterialDialog dialog, Integer[] which, CharSequence[] text) {
+                                String value = "";
+                                for (int i = 0; i < text.length; i++) {
+                                    if (i == 0) {
+                                        value = text[0].toString();
+                                    } else {
+                                        value = value + "," + text[i].toString();
+                                    }
+                                    etLocation.setText(value);
                                     //Log.e("Text",""+text[i].toString()+",");
                                 }
                                 return true;
@@ -179,6 +223,11 @@ public class FilterActivity extends AppCompatActivity {
 
 
     }
+
+
+
+
+
 
 
 }
